956261ec98fbbd6417777de1b9c183f1c2a7814c
Niv Alfandari <niv.a2098@gmail.com>
2024-09-10 19:34:35 +0300

    simulator.h file changes

--- include/simulator/simulator.h
+++ include/simulator/simulator.h
@@ -1,27 +1,25 @@
+//
+// Created by tzuk on 6/4/23.
+//
+
 #ifndef ORB_SLAM2_SIMULATOR_H
 #define ORB_SLAM2_SIMULATOR_H
 
 #include <memory>
-#include <Eigen/SVD>
-#include <filesystem>
-#include <functional>
-#include <pangolin/gl/glsl.h>
 #include <pangolin/pangolin.h>
-#include <pangolin/gl/glvbo.h>
-#include <pangolin/display/display.h>
-#include <pangolin/utils/file_utils.h>
 #include <pangolin/geometry/geometry.h>
+#include <pangolin/gl/glsl.h>
+#include <pangolin/gl/glvbo.h>
+#include <functional>
+#include <pangolin/display/display.h>
 #include <pangolin/display/pangolin_gl.h>
+#include <pangolin/utils/file_utils.h>
 #include <pangolin/geometry/glgeometry.h>
-
 #include "ORBextractor.h"
 #include "System.h"
-
-#include "RunModel/TextureShader.h"
-#include "Auxiliary.h"
-
-
-
+#include <Eigen/SVD>
+#include <filesystem>
+#include "include/run_model/TextureShader.h"
 /**
  *  @class Simulator
  *  @brief This class provides a simulation environment for virtual robotic navigation and mapping.
@@ -64,8 +62,8 @@ public:
      *
      * @param vocPath: A string representing the path to the ORBSLAM2 vocabulary file. Defaults to "../Vocabulary/ORBvoc.txt" if not specified.
      */
-    Simulator(std::string ORBSLAMConfigFile, std::string model_path, bool alignModelToTexture, std::string modelTextureNameToAlignTo,
-              Eigen::Vector3f startingPoint, bool saveMap = false, std::string simulatorOutputDirPath = "../slamMaps/", bool loadMap = false,
+    Simulator(std::string ORBSLAMConfigFile, std::string model_path, bool alignModelToTexture, std::string modelTextureNameToAlignTo, bool trackImages = true,
+              bool saveMap = false, std::string simulatorOutputDirPath = "../slamMaps/", bool loadMap = false,
               std::string mapLoadPath = "../slamMaps/example.bin",
               double movementFactor = 0.01,
               double speedFactor = 1.0,
@@ -74,7 +72,7 @@ public:
     /**
      *Starts the 3D model viewer (pangolin), and wait for the user or code signal to start sending the view to the ORBSLAM2 object
      * */
-    std::thread run();
+    std::thread run(const std::string& modelName, const std::string& displayName);
     /**
      * sample the simulator state, this changes from false to true once the model is loaded
      * @return is the simulator is ready
@@ -91,17 +89,7 @@ public:
      * - The last column represents the translation vector.
      * - The y-axis indicates the height in reverse (i.e., negative values correspond to upward direction).
      */
-    cv::Mat getCurrentLocationSlam();
-    /**
-     * @brief Fetches the current location matrix from Pangolin.
-     *
-     * @return A 4x4 location matrix where:
-     * - The first 3x3 sub-matrix represents the rotation matrix.
-     * - The last column represents the translation vector.
-     * - The y-axis indicates the height in reverse (i.e., negative values correspond to upward direction).
-
-     */
-    Eigen::Matrix4d getCurrentLocation();
+    cv::Mat getCurrentLocation();
     /**
      * @brief Retrieves the current map from ORBSLAM2.
      *
@@ -131,19 +119,20 @@ public:
      * @brief enabling or disabling the ORBSLAM process.
      *
      */
-    void setTrack(bool value);
+    void setTrack(bool value) { track = value; }
 
     void setSpeed(double speed);
 
     double getSpeed() const;
 
-    void simulatorRunThread();
+    void simulatorRunThread_orig(const std::string& modelName, const std::string& displayName);
+    void simulatorRunThread_orig2();
+    void simulatorRunThread_two(const std::string& modelName, const std::string& displayName);
+    void simulatorRunThread(const std::string& modelName, const std::string& displayName);
+    void simulatorRunThread_multi(const std::string& modelName, const std::string& displayName);
+    void simulatorRunThread_nerf(const std::string& modelName, const std::string& displayName);
 
     std::shared_ptr<ORB_SLAM2::System> GetSLAM() { return SLAM; }
-
-    void drawPoint(cv::Point3d point, float size, Eigen::Vector3d color);
-
-    void cleanPoints();
 
 private:
     /**
@@ -165,10 +154,6 @@ private:
             {"flip", false},
             {"rc", false}};
     std::shared_ptr<ORB_SLAM2::System> SLAM;
-    std::string vocPath;
-    std::string ORBSLAMConfigFile;
-    std::string mapLoadPath;
-    bool loadMap;
     pangolin::OpenGlRenderState s_cam;
     Eigen::Matrix3d K;
     std::shared_ptr < ORB_SLAM2::ORBextractor> orbExtractor;
@@ -177,11 +162,7 @@ private:
     bool stopFlagSLAM;
     bool ready;
     bool start;
-    bool initSlam;
 
-    std::vector<std::pair<cv::Point3d, std::pair<float, Eigen::Vector3d>>> points;
-
-    Eigen::Vector3f startingPoint;
     bool saveMapSignal;
     bool track;
     double movementFactor{};
@@ -192,6 +173,7 @@ private:
     bool isSaveMap;
     bool isInitalized;
     bool isLocalized;
+    bool trackImages;
     bool cull_backfaces;
     pangolin::GlSlProgram program;
     pangolin::GlGeometry geomToRender;
@@ -206,7 +188,7 @@ private:
 
     void SLAMThread();
 
-    bool feedSLAM(cv::Mat &img);
+    bool feedSLAM(cv::Mat& img);
 
     void extractSurface(const pangolin::Geometry &modelGeometry, std::string modelTextureNameToAlignTo,
                         Eigen::MatrixXf &surface);
